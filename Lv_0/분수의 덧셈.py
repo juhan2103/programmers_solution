9828e3557c2414418b6c886558939b54025b95a6
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-08-23 14:46:40 +0900

    [Lv0] 분수의 덧셈

---
+++ Lv_0/분수의 덧셈.py
@@ -0,0 +1,6 @@
+import math
+def solution(numer1, denom1, numer2, denom2):
+    numer = denom1 * numer2 + denom2 * numer1
+    denom = denom1 * denom2
+    gcd = math.gcd(numer, denom)
+    return [numer//gcd, denom//gcd]
