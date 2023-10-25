673e908ec52e141dee5c939529d370255cc4d79b
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-10-25 16:47:59 +0900

    [Lv0] 모음 제거

---
+++ Lv_0/모음 제거.py
@@ -0,0 +1,6 @@
+def solution(my_string):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    for i in my_string:
+        if i in vowels:
+            my_string = my_string.replace(i, '')
+    return my_string
